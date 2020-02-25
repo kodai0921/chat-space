39992c5385da4531409a222022cafab1bf5b1cdd
kodai0921 <k.kitahara51@gmail.com>
2020-02-25 19:46:29 +0900

    非同期通信実装

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,14 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.test_framework false
       config.i18n.default_locale = :ja
+      config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     end
   end
 end
